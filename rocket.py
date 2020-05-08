df5f710cab332be6c879982a9d256d1de0f5ad18
hudaszekpiotr <61946903+hudaszekpiotr@users.noreply.github.com>
2020-05-08 15:27:35 +0200

    menu

--- rocket.py
+++ rocket.py
@@ -1,8 +1,8 @@
-import pygame
-class Rocket(object):
-    def __init__(self,game):
-        self.game_main = game
-    def tick(self):
-        pass
-    def draw(self):
-        pass
+#import pygame
+#class Rocket(object):
+#    def __init__(self,game):
+#        self.game_main = game
+#    def tick(self):
+#        pass
+#    def draw(self):
+#        pass
